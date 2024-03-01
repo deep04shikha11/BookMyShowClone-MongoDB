f576d1e22184f2630d42826067ae001160daaae0
deepshikha <deepshikhaverma44@gmail.com>
2024-03-01 21:58:20 +0530

    Password hashing

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const PORT = 8050;
 require('dotenv').config();
 const DbConfig = require('./config/dbConfig');
 
+app.use(express.json());
+
+const userRoutes = require('../server/routes/userRoutes');
+
+app.use('/api/users', userRoutes);
+
 app.listen(PORT, () => {
     console.log('Server is running at Port', +PORT);
 })
